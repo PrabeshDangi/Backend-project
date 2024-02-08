8ec63016547b54a5227bab62b853a2ab5b53e906
Prabesh Dangi <dangiprabesh58@gmail.com>
2024-02-08 21:13:17 +0545

    utils configured

---
+++ src/utils/responseError.js
@@ -0,0 +1,8 @@
+class responseError {
+  constructor(statusCode, data, message = "Success") {
+    (this.statusCode = statusCode),
+      (this.message = message),
+      (this.data = data),
+      (this.success = statusCode < 400);
+  }
+}
